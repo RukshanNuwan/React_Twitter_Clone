0711241b3fd3eb2e8b295235d611549552236c5f
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-02-04 22:20:18 +0530

    deployed

--- src/component/Feed.js
+++ src/component/Feed.js
@@ -2,15 +2,16 @@ import {useEffect, useState} from "react";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import {collection, onSnapshot, query, orderBy} from 'firebase/firestore';
-import './Feed.css';
 import {db} from "../firebase";
+import FlipMove from "react-flip-move";
+import './Feed.css';
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const colRef = collection(db, 'posts');
-    const q = query(colRef, orderBy('timeStamp','desc'));
+    const q = query(colRef, orderBy('timeStamp', 'desc'));
 
     onSnapshot(q, (snapshot) => {
       setPosts(snapshot.docs.map(doc => doc.data()));
@@ -25,28 +26,20 @@ const Feed = () => {
 
       <TweetBox/>
 
-      {posts.map((post, index) => (
-        <Post
-          key={index}
-          avatar={post.avatar}
-          displayName={post.displayName}
-          userName={post.userName}
-          image={post.image}
-          text={post.text}
-          isVerified={post.isVerified}
-          timeStamp={post.timeStamp}
-        />
-      ))}
-
-      {/*<Post*/}
-      {/*  avatar="https://avatars.githubusercontent.com/u/39560159?v=4"*/}
-      {/*  displayName="Rukshan Nuwan"*/}
-      {/*  userName="rukshannuwan"*/}
-      {/*  image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"*/}
-      {/*  text="I challenge you to build a twitter clone with react"*/}
-      {/*  isVerified={true}*/}
-      {/*  timeStamp="8h"*/}
-      {/*/>*/}
+      <FlipMove>
+        {posts.map((post, index) => (
+          <Post
+            key={index}
+            avatar={post.avatar}
+            displayName={post.displayName}
+            userName={post.userName}
+            image={post.image}
+            text={post.text}
+            isVerified={post.isVerified}
+            timeStamp={post.timeStamp}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 };
--- src/component/Post.js
+++ src/component/Post.js
@@ -1,47 +1,49 @@
-import './Post.css';
+import {forwardRef} from "react";
 import {Avatar} from "@mui/material";
 import {ChatBubbleOutline, FavoriteBorder, Publish, Repeat, Verified} from "@mui/icons-material";
+import './Post.css';
 
-const Post = ({displayName, userName, isVerified, timeStamp, text, image, avatar}) => {
-  return (
-    <div className="post">
-      <div className="post__avatar">
-        <Avatar src={avatar}/>
-      </div>
+const Post = forwardRef(
+  ({displayName, userName, isVerified, timeStamp, text, image, avatar}, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar}/>
+        </div>
 
-      <div className="post__body">
-        <div className="post__header">
-          <div className="post__headerText">
-            <h3>
-              {displayName}
-              <span className="post__headerSpecial">
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}
+                <span className="post__headerSpecial">
                 {isVerified && <Verified className="post__badge"/>}
-                @{userName} 1h ago
+                  @{userName} 1h ago
               </span>
-            </h3>
+              </h3>
+            </div>
+
+            <div className="post__headerDescription">
+              <p>
+                {text}
+                {/*  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae eum, excepturi fuga ipsam magnam*/}
+                {/*  maxime molestias nulla numquam officia optio perferendis quis sed similique soluta sunt tenetur, veritatis*/}
+                {/*  voluptate.*/}
+              </p>
+            </div>
           </div>
 
-          <div className="post__headerDescription">
-            <p>
-              {text}
-              {/*  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae eum, excepturi fuga ipsam magnam*/}
-              {/*  maxime molestias nulla numquam officia optio perferendis quis sed similique soluta sunt tenetur, veritatis*/}
-              {/*  voluptate.*/}
-            </p>
+          <img src={image} alt=""/>
+
+          <div className="post__footer">
+            <ChatBubbleOutline fontSize="small"/>
+            <Repeat fontSize="small"/>
+            <FavoriteBorder fontSize="small"/>
+            <Publish fontSize="small"/>
           </div>
-        </div>
-
-        <img src={image} alt=""/>
-
-        <div className="post__footer">
-          <ChatBubbleOutline fontSize="small"/>
-          <Repeat fontSize="small"/>
-          <FavoriteBorder fontSize="small"/>
-          <Publish fontSize="small"/>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  });
 
 export default Post;
